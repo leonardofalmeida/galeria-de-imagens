3e1e450c21da88ea8cf484df92c92f87c553852d
leonardofalmeida <ferreiraleoalmeida@gmail.com>
2019-10-06 19:24:18 -0300

    Commit inicial com estrutura

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+
+    <ul>
+      <li v-for="foto of fotos" :key="foto.titulo">
+        <img :src="foto.url" :alt="foto.titulo" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titulo: "Galeria de Ibagens",
+      fotos: [
+        {
+          url:
+            "https://www.petz.com.br/blog/wp-content/uploads/2019/04/como-saber-se-o-cachorro-esta-com-febre-doencas.jpg",
+          titulo: "Cachorro"
+        },
+        {
+          url:
+            "https://www.petz.com.br/blog/wp-content/uploads/2019/04/como-saber-se-o-cachorro-esta-com-febre-doencas.jpg",
+          titulo: "Cachorrinho"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
